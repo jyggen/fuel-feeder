1235109808118388411f8ebd8b483bd9e8bc7dea
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-08-17 00:59:59 +0200

    Added bootstrap file with autoloaded Feed class.

---
+++ bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Feeder: A Feed/RSS generator for FuelPHP.
+ *
+ * @package		Feeder
+ * @version		1.0
+ * @author		Jonas Stendahl
+ * @license		MIT License
+ * @copyright	2012 Jonas Stendahl
+ * @link		https://github.com/jyggen/fuel-feeder
+ */
+
+Autoloader::add_classes(array(
+	'Feeder\\Feed' => __DIR__.'/classes/feed.php',
+));
