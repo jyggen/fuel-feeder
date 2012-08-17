4eef970c4dd06202e448fe86e0012419c6fc5f9e
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-08-17 19:32:05 +0200

    Added Item class.

---
+++ classes/item.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Feeder: A Feed/RSS generator for FuelPHP.
+ *
+ * @package		Feeder
+ * @version		1.0
+ * @author		Jonas Stendahl
+ * @license		MIT License
+ * @copyright	2012 Jonas Stendahl
+ * @link		https://github.com/jyggen/fuel-feeder
+ */
+
+namespace Feeder;
+
+class Item
+{
+
+	public static function forge($driver='rss2')
+	{
+
+		$class  = 'Feeder\\Item_'.ucfirst($driver);
+		$driver = new $class();
+
+		return $driver;
+
+	}
+
+}
