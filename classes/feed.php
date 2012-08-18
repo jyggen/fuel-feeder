21613b1d58c105e30d5a6b2ed5c796bc77e58077
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-08-18 02:36:04 +0200

    Removed pre-driver code from Feed.

--- classes/feed.php
+++ classes/feed.php
@@ -12,12 +12,15 @@
 
 namespace Feeder;
 
-
-class MissingTagException extends \FuelException {}
-
-
 class Feed
 {
+
+	public static function _init()
+	{
+
+		\Config::load('feeder', true);
+
+	}
 
 	public static function forge($driver='rss2')
 	{
@@ -26,49 +29,6 @@ class Feed
 		$driver = new $class();
 
 		return $driver;
-
-	}
-
-	/**
-	 * Helper to set a value for the attribute "lastBuildDate". Automatically generated if missing.
-	 *
-	 * @param	\Date	$value
-	 * @return	void
-	 */
-	public function buildDate(\Date $value)
-	{
-
-		/**
-		 * Would love to use Date::format() here, but %z in strftime is weird on Windows.
-		 * Also, the DATE_RSS constant is pretty nice :)
-		 */
-		$this->setAttr('lastBuildDate', date(DATE_RSS, $value->get_timestamp()));
-
-	}
-
-	/**
-	 * Helper to set a value for the attribute "description".
-	 *
-	 * @param	string	$value
-	 * @return	void
-	 */
-	public function description($value)
-	{
-
-		$this->setAttr('description', $value);
-
-	}
-
-	/**
-	 * Helper to set a value for the attribute "title".
-	 *
-	 * @param	string	$value
-	 * @return	void
-	 */
-	public function title($value)
-	{
-
-		$this->setAttr('title', $value);
 
 	}
 
