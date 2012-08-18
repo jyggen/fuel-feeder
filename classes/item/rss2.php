24c942f4ba8791a8d8e7db6ed42930ef9163f781
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-08-18 02:36:45 +0200

    Added more helper methods to Item_Rss2.

--- classes/item/rss2.php
+++ classes/item/rss2.php
@@ -15,8 +15,95 @@ namespace Feeder;
 class Item_Rss2 extends Item_Driver
 {
 
+	protected function createBaseElement()
+	{
+
+		$element = $this->document->createElement('item');
+
+		return $element;
+
+	}
+
 	/**
-	 * Create <link> in <item>. Should be the link to your item.
+	 * Create <category> in <item>.
+	 *
+	 * @param	string	$value
+	 * @return	void
+	 */
+	public function category($value)
+	{
+
+		$this->addTag('category', $value, array(), true);
+
+	}
+
+	/**
+	 * Create <comments> in <item>.
+	 *
+	 * @param	string	$value
+	 * @return	void
+	 */
+	public function comments($value)
+	{
+
+		$this->addTag('comments', $value);
+
+	}
+
+	/**
+	 * Create <content:encoded> in <item>.
+	 *
+	 * @param	string	$value
+	 * @return	void
+	 */
+	public function contentEncoded($value)
+	{
+
+		$this->addTag('content:encoded', $value, array(), true);
+
+	}
+
+	/**
+	 * Create <dc:creator> in <item>.
+	 *
+	 * @param	string	$value
+	 * @return	void
+	 */
+	public function dcCreator($value)
+	{
+
+		$this->addTag('dc:creator', $value);
+
+	}
+
+	/**
+	 * Create <description> in <item>.
+	 *
+	 * @param	string	$value
+	 * @return	void
+	 */
+	public function description($value)
+	{
+
+		$this->addTag('description', $value, array(), true);
+
+	}
+
+	/**
+	 * Create <guid> in <item>.
+	 *
+	 * @param	string	$value
+	 * @return	void
+	 */
+	public function guid($value, $isPermaLink=false)
+	{
+
+		$this->addTag('guid', $value, array('isPermaLink' => $isPermaLink));
+
+	}
+
+	/**
+	 * Create <link> in <item>.
 	 *
 	 * @param	string	$value
 	 * @return	void
@@ -29,7 +116,37 @@ class Item_Rss2 extends Item_Driver
 	}
 
 	/**
-	 * Create <title> in <item>. Should be the title of your item.
+	 * Create <pubDate> in <item>.
+	 *
+	 * @param	\Date	$value
+	 * @return	void
+	 */
+	public function pubDate(\Date $value)
+	{
+
+		/**
+		 * Would love to use Date::format() here, but %z in strftime is weird on Windows.
+		 * Also, the DATE_RSS constant is pretty nice :)
+		 */
+		$this->addTag('pubDate', date(DATE_RSS, $value->get_timestamp()));
+
+	}
+
+	/**
+	 * Create <slash:comments> in <item>.
+	 *
+	 * @param	string	$value
+	 * @return	void
+	 */
+	public function slashComments($value)
+	{
+
+		$this->addTag('slash:comments', $value);
+
+	}
+
+	/**
+	 * Create <title> in <item>.
 	 *
 	 * @param	string	$value
 	 * @return	void
@@ -41,12 +158,16 @@ class Item_Rss2 extends Item_Driver
 
 	}
 
-	protected function createElement()
+	/**
+	 * Create <wfw:commentRss> in <item>.
+	 *
+	 * @param	string	$value
+	 * @return	void
+	 */
+	public function wfwCommentRss($value)
 	{
 
-		$element = $this->document->createElement('item');
-
-		return $element;
+		$this->addTag('wfw:commentRss', $value);
 
 	}
 
