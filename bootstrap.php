eaccca50c002e788694e1331e15279899949ce42
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-08-23 03:38:30 +0200

    Added classes for Atom driver to autoloader

--- bootstrap.php
+++ bootstrap.php
@@ -13,9 +13,11 @@
 Autoloader::add_classes(array(
 	'Feeder\\Driver'      => __DIR__.'/classes/driver.php',
 	'Feeder\\Feed'        => __DIR__.'/classes/feed.php',
+	'Feeder\\Feed_Atom'   => __DIR__.'/classes/feed/atom.php',
 	'Feeder\\Feed_Driver' => __DIR__.'/classes/feed/driver.php',
 	'Feeder\\Feed_Rss2'   => __DIR__.'/classes/feed/rss2.php',
 	'Feeder\\Item'        => __DIR__.'/classes/item.php',
+	'Feeder\\Item_Atom'   => __DIR__.'/classes/item/atom.php',
 	'Feeder\\Item_Driver' => __DIR__.'/classes/item/driver.php',
 	'Feeder\\Item_Rss2'   => __DIR__.'/classes/item/rss2.php',
 ));
