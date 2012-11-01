6dccdd07142493134e548d611114babd556b1e58
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-11-02 00:29:24 +0100

    Added test files.

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+ini_set('display_errors', '1');
+
+require_once '../classes/feed.php';
+require_once '../classes/item.php';
+require_once '../classes/driver.php';
+require_once '../classes/feed/driver.php';
+require_once '../classes/feed/atom.php';
+require_once '../classes/feed/rss2.php';
+require_once '../classes/item/driver.php';
+require_once '../classes/item/atom.php';
+require_once '../classes/item/rss2.php';
---
+++ tests/feeder.php
@@ -0,0 +1,14 @@
+<?php
+namespace Feeder;
+
+class Test_Feeder extends \PHPUnit_Framework_TestCase
+{
+
+	public function test_forge()
+	{
+
+		$this->assertFalse(false);
+
+	}
+
+}
